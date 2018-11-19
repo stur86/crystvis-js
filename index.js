93ce7a504cf9f32734829ace7f6cac08e8c7edbe
Simone Sturniolo <simone.sturniolo@gmail.com>
2018-11-19 15:58:31 +0000

    Created basic Renderer object

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var $ = require('jquery');
+
+var Renderer = require('./lib/render.js').Renderer;
+
+// Bootstrap the whole thing!
+$(document).ready(function() {
+    var r = new Renderer('.main-app-content');
+});
