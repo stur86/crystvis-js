a53de1c59be6ab0efc1b0af8dec8f72e3722adfa
stur86 <simonesturniolo@gmail.com>
2018-11-20 17:32:00 +0000

    Added atoms and bonds rendering

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ var Renderer = require('./lib/render.js').Renderer;
 // Bootstrap the whole thing!
 $(document).ready(function() {
     var r = new Renderer('.main-app-content', 640, 480);
-    r.test();
+    r._addAtom('Atom', [0, 0, 1], 0.5, 0xff0000);
+    r._addAtom('Atom', [0.9, 0, -0.2], 0.35, 0xeeeeee);
+    r._addAtom('Atom', [-0.9, 0, -0.2], 0.35, 0xeeeeee);
+
+    r._addBond('AB', [0, 0, 1], [0.9, 0, -0.2], 0.2, 0xff0000, 0xeeeeee);
+    r._addBond('AB', [0, 0, 1], [-0.9, 0, -0.2], 0.2, 0xff0000, 0xeeeeee);
 });
