c8afdecfe924318301a9e26cad0f2586f55a3bfd
stur86 <simonesturniolo@gmail.com>
2019-04-11 12:00:45 +0100

    Setting up Mocha testing

--- test/test_rmatrix.js
+++ test/test_rmatrix.js
@@ -1,11 +1,17 @@
-var Atoms = require('crystcif-parse').Atoms;
-var fs = require('fs');
+'use strict';
+
+var expect = require('chai').expect;
+
+const Atoms = require('crystcif-parse').Atoms;
+const path = require('path');
+const fs = require('fs');
 var nm = require('numeric');
 
-var cif = fs.readFileSync('CHA.cif', "utf8");
+var cif = fs.readFileSync(path.join(__dirname, 'CHA.cif'), "utf8");
 var a = Atoms.readCif(cif)['CHA'];
 
-var c = a.get_cell();
+var c = new nm.T(a.get_cell());
+var rmat = c.dot(c.transpose());
 
-console.log(c);
-console.log(nm.dot(c, nm.transpose(c)));
+console.log(rmat);
+// console.log(nm.dot(c, nm.transpose(c)));
