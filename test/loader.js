16d00836e9b914496cba195b8e0602a55e547811
stur86 <simonesturniolo@gmail.com>
2019-04-12 16:49:51 +0100

    Implemented Extended XYZ load test

--- test/loader.js
+++ test/loader.js
@@ -17,13 +17,14 @@ describe('#loading', function () {
         expect(a.get_chemical_symbols()).to.deep.equal(['C', 'C', 'C', 'N', 'C', 'C',
             'H', 'H', 'H', 'H', 'H']);
     });
-    it('should load properly an extended XYZ file', function() {
+    it('should load properly an extended XYZ file', function () {
 
         var loader = new Loader();
 
         var xyz = fs.readFileSync(path.join(__dirname, 'data', 'si8.xyz'), "utf8");
         var a = loader.loadXYZ(xyz);
 
-        // console.log(a);
+        expect(a.get_cell()).to.deep.equal([[5.44, 0, 0], [0, 5.44, 0], [0, 0, 5.44]]);
+        expect(a.get_array('spin')).to.deep.equal([1, 0, 0, 0, 0, 0, 0, 0]);
     });
 });
