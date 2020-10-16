d92f80ec034c68910b728d47ec8fbfb2f113b0bf
stur86 <simonesturniolo@gmail.com>
2020-10-16 12:15:11 +0100

    Added test for loadCIF

--- test/loader.js
+++ test/loader.js
@@ -1,13 +1,17 @@
 'use strict';
 
-const expect = require('chai').expect;
+const chai = require('chai');
+const chaiAlmost = require('chai-almost');
+const expect = chai.expect
 
 const fs = require('fs');
 const path = require('path');
 const Loader = require('../lib/loader.js').Loader;
 
-describe('#loading', function () {
-    it('should load properly an XYZ file', function () {
+chai.use(chaiAlmost(1e-3));
+
+describe('#loading', function() {
+    it('should load properly an XYZ file', function() {
 
         var loader = new Loader();
 
@@ -15,16 +19,35 @@ describe('#loading', function () {
         var a = loader.loadXYZ(xyz);
 
         expect(a.get_chemical_symbols()).to.deep.equal(['C', 'C', 'C', 'N', 'C', 'C',
-            'H', 'H', 'H', 'H', 'H']);
+            'H', 'H', 'H', 'H', 'H'
+        ]);
     });
-    it('should load properly an extended XYZ file', function () {
+    it('should load properly an extended XYZ file', function() {
 
         var loader = new Loader();
 
         var xyz = fs.readFileSync(path.join(__dirname, 'data', 'si8.xyz'), "utf8");
         var a = loader.loadXYZ(xyz);
 
-        expect(a.get_cell()).to.deep.equal([[5.44, 0, 0], [0, 5.44, 0], [0, 0, 5.44]]);
+        expect(a.get_cell()).to.deep.almost.equal([
+            [5.44, 0, 0],
+            [0, 5.44, 0],
+            [0, 0, 5.44]
+        ]);
         expect(a.get_array('spin')).to.deep.equal([1, 0, 0, 0, 0, 0, 0, 0]);
+    });
+    it('should load properly a CIF file', function() {
+
+        var loader = new Loader();
+
+        var cif = fs.readFileSync(path.join(__dirname, 'data', 'org.cif'), "utf8");
+        var a = loader.loadCIF(cif)['1501936'];
+
+
+        chai.expect(a.get_cell()).to.deep.almost.equal([
+            [8.2302, 0.0, 0.0],
+            [1.7507207912272031, 8.096583378263006, 0.0],
+            [1.7848106706548383, 1.959874486240255, 12.914460443940394]
+        ]);
     });
 });
