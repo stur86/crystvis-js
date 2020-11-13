47be432e68c4c8a10d73304deeda98ba90da6cb3
stur86 <simonesturniolo@gmail.com>
2020-11-13 16:29:02 +0000

    Created a demo page, testing various functionality

---
+++ demo/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const CrystVis = require('../lib/visualizer.js').CrystVis;
+
+var visualizer = new CrystVis('#main-app', {'width': 640, 'height': 480});
+
+window.loadFile = function() {
+    var file = document.getElementById('file-load').files[0];
+    var reader = new FileReader();
+    var extension = file.name.split('.').pop();
+    
+    reader.readAsText(file);
+    reader.onload = function() {
+        var name = file.name.split('.')[0];
+        visualizer.loadModels(reader.result, extension, name);
+        visualizer.displayModel(name);
+    };
+}
